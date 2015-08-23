eeb93657655f5ca2237994b506b27eb436f57b24
Matthew <matthewgao@gmail.com>
2015-08-23 15:38:44 +0800

    fix shell bug

--- setup.sh
+++ setup.sh
@@ -19,7 +19,7 @@ cd ~/ && git clone https://github.com/matthewgao/MyVim.git
 mv -f ~/.vim ~/.vim_old
 mv -f ~/MyVim/vim ~/.vim
 mv -f ~/.vimrc ~/.vimrc_old
-mv -f ~/MyVim/vimrc .vimrc ~/
+mv -f ~/MyVim/vimrc  ~/.vimrc
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 echo "Installing Bundle program, please wati for a while" > matthewgao
 echo "After the installation complete it will quit automatically" >> matthewgao
