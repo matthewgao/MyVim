0a6bc75885a16912960b0f084f413c7f213b75e6
Matthew <matthewgao@gmail.com>
2015-08-24 20:56:40 +0800

    refine the script

--- setup.sh
+++ setup.sh
@@ -21,27 +21,25 @@ if which apt-get >/dev/null; then
     sudo apt-get install -y vim vim-gnome ctags xclip astyle python-setuptools python-dev git
 elif which yum >/dev/null; then
     sudo yum install -y gcc vim git ctags xclip astyle python-setuptools python-devel   
-fi
-
 ##Add HomeBrew support on  Mac OS
-if which brew >/dev/null;then
+elif which brew >/dev/null;then
     success "You are using HomeBrew tool"
     brew install vim --with-lua
     brew install ctags git astyle
     success "Install successfully"
+else
+    error "Install Vim and other tools FAIL!"
 fi
 
 sudo easy_install -ZU autopep8 
 sudo ln -s /usr/bin/ctags /usr/local/bin/ctags
 mv -f ~/vim ~/vim_old
 cd ~/ && git clone https://github.com/matthewgao/MyVim.git
-mv -f ~/.vim ~/.vim_old
-mkdir .vim
-cp -R ~/MyVim/vim/* ~/.vim
-mv -f ~/.vimrc ~/.vimrc_old
-cp ~/MyVim/vimrc  ~/.vimrc
+mv -f ~/.vim ~/.vim_old && mkdir .vim && cp -R ~/MyVim/vim/* ~/.vim
+mv -f ~/.vimrc ~/.vimrc_old && cp ~/MyVim/vimrc  ~/.vimrc
 cd ~/MyVim/vimcdoc-1.9.0/ && ./vimcdoc.sh -i
 cd ~/ && git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-vim  -c "BundleInstall" -c "q" -c "q"
+vim  -c "BundleInstall" -c "qa"
 rm -rf MyVim
-success "ALL Set!"
+
+success "ALL Set!, Start your Vim adventure~"
