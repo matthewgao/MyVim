953f5927ef7bbe25a6df0d60d6a46a2a0ceac8b7
Matthew <matthewgao@gmail.com>
2015-08-24 17:54:02 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-msg() {
-    printf '%b\n' "$1" >&2
-}
+#msg() {
+#    printf '%b\n' "$1" >&2
+#}
+#
+#success() {
+#    if [ "$ret" -eq '0' ]; then
+#        msg "\33[32m[✔]\33[0m ${1}${2}"
+#    fi
+#}
+#
+#error() {
+#    msg "\33[31m[✘]\33[0m ${1}${2}"
+#    exit 1
+#}
 
-success() {
-    if [ "$ret" -eq '0' ]; then
-        msg "\33[32m[✔]\33[0m ${1}${2}"
-    fi
-}
-
-error() {
-    msg "\33[31m[✘]\33[0m ${1}${2}"
-    exit 1
-}
-
-ret="$?"
-success "Installing....., Please wait for a moment"
+#ret="$?"
+echo "Installing....., Please wait for a moment"
 if which apt-get >/dev/null; then
     sudo apt-get install -y vim vim-gnome ctags xclip astyle python-setuptools python-dev git
 elif which yum >/dev/null; then
@@ -25,10 +25,10 @@ fi
 
 ##Add HomeBrew support on  Mac OS
 if which brew >/dev/null;then
-    success "You are using HomeBrew tool"
+    echo "You are using HomeBrew tool"
     brew install vim --with-lua
     brew install ctags git astyle
-    success "Install successfully"
+    echo "Install successfully"
 fi
 
 sudo easy_install -ZU autopep8 
@@ -44,4 +44,4 @@ cd ~/MyVim/vimcdoc-1.9.0/ && ./vimcdoc.sh -i
 cd ~/ && git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim  -c "BundleInstall" -c "q" -c "q"
 rm -rf MyVim
-success "ALL Set!"
+echo "ALL Set!"
